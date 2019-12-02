664b418de33523b48a1c8d64d90050afb8044478
huangshuchao <huangshuchao1992>
2019-12-02 23:02:38 +0800

    新建项目 commit

---
+++ src/main/java/com/home/homemanage/HomemanageApplication.java
@@ -0,0 +1,13 @@
+package com.home.homemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomemanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomemanageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/home/homemanage/HomemanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.homemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomemanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
